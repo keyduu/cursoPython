37f0681dc1fe5b5bd1cbd3af8a71e7012b75457f
keyduu <keyduu@gmail.com>
2020-03-30 07:33:57 +0200

    Subidos los archivos que faltaban.

---
+++ conversor_monedas/main.py
@@ -0,0 +1,57 @@
+'''
+# +Sobre la actual aplicacion. Incorporar 1 boton para transformar los euros introducidos
+# a otra moneda a elegir por el programador.
+# + Incorporar un boton más para tranformar a otro tipo de moneda distinto
+# + Mostrar un icono en el boton correspondiente de conversion
+
+'''
+import sys
+
+# ==== IMPORTS ==== #
+from PyQt5 import QtWidgets
+from ventanas import ventana_python
+
+# ==== VARIABLES GLOBALES ==== #
+conversion_dolares = 1.11
+conversion_yenes = 119.15
+
+
+# ==== FUNCIONES ==== #
+def transformar_a_dolares():
+    introducido = ui.edit_euros.text()
+    introducido_float = float(introducido.replace(",", "."))
+    dolares = introducido_float * conversion_dolares
+    ui.label_resultado.setText("Cantidad(dolares): ")
+    ui.edit_resultado.setText("{:.2f}".format(dolares).replace(".", ","))
+
+
+def transformar_a_yenes():
+    introducido = ui.edit_euros.text()
+    introducido_float = float(introducido.replace(",", "."))
+    yenes = introducido_float * conversion_yenes
+    ui.label_resultado.setText("Cantidad(yenes): ")
+    ui.edit_resultado.setText("{:.2f}".format(yenes).replace(".", ","))
+
+
+# ==== MAIN ==== #
+
+# vamos a usar el archivo generado de la ventana directamente
+
+# linea obligatoria para usar pyqt5
+app = QtWidgets.QApplication(sys.argv)
+
+# se prepara un MainWindow de pyqt5, esto seria parte del codigo recomendado de pyqt5
+MainWindow = QtWidgets.QMainWindow()
+
+# asi crea un objeto de la clase en el archivo generado y lo usa para preparar la ventana principal
+# llamada MainWindow para que tenga todo lo que pusimos en el designer
+ui = ventana_python.Ui_MainWindow()
+ui.setupUi(MainWindow)
+
+# todos los componentes puestos en la ventana por el designer estan ui
+ui.boton_convertir_dolares.clicked.connect(transformar_a_dolares)
+ui.boton_convertir_yenes.clicked.connect(transformar_a_yenes)
+
+# se muestra la ventana principal de PyQt5
+MainWindow.show()
+sys.exit(app.exec_())
---
+++ conversor_monedas/ventanas/ventana_python.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ventana_principal.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(530, 179)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.boton_convertir_dolares = QtWidgets.QPushButton(self.centralwidget)
+        self.boton_convertir_dolares.setGeometry(QtCore.QRect(10, 90, 250, 80))
+        font = QtGui.QFont()
+        font.setFamily("Verdana")
+        font.setPointSize(14)
+        self.boton_convertir_dolares.setFont(font)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("coin-dollar.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.boton_convertir_dolares.setIcon(icon)
+        self.boton_convertir_dolares.setIconSize(QtCore.QSize(32, 32))
+        self.boton_convertir_dolares.setObjectName("boton_convertir_dolares")
+        self.boton_convertir_yenes = QtWidgets.QPushButton(self.centralwidget)
+        self.boton_convertir_yenes.setGeometry(QtCore.QRect(270, 90, 250, 80))
+        font = QtGui.QFont()
+        font.setFamily("Verdana")
+        font.setPointSize(14)
+        self.boton_convertir_yenes.setFont(font)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("coin-yen.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.boton_convertir_yenes.setIcon(icon1)
+        self.boton_convertir_yenes.setIconSize(QtCore.QSize(32, 32))
+        self.boton_convertir_yenes.setObjectName("boton_convertir_yenes")
+        self.edit_euros = QtWidgets.QLineEdit(self.centralwidget)
+        self.edit_euros.setGeometry(QtCore.QRect(270, 10, 250, 30))
+        font = QtGui.QFont()
+        font.setFamily("Verdana")
+        font.setPointSize(18)
+        self.edit_euros.setFont(font)
+        self.edit_euros.setObjectName("edit_euros")
+        self.edit_resultado = QtWidgets.QLineEdit(self.centralwidget)
+        self.edit_resultado.setEnabled(False)
+        self.edit_resultado.setGeometry(QtCore.QRect(270, 50, 250, 30))
+        font = QtGui.QFont()
+        font.setFamily("Verdana")
+        font.setPointSize(18)
+        self.edit_resultado.setFont(font)
+        self.edit_resultado.setObjectName("edit_resultado")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 10, 250, 30))
+        font = QtGui.QFont()
+        font.setFamily("Verdana")
+        font.setPointSize(18)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label.setObjectName("label")
+        self.label_resultado = QtWidgets.QLabel(self.centralwidget)
+        self.label_resultado.setGeometry(QtCore.QRect(10, 50, 250, 30))
+        font = QtGui.QFont()
+        font.setFamily("Verdana")
+        font.setPointSize(18)
+        self.label_resultado.setFont(font)
+        self.label_resultado.setText("")
+        self.label_resultado.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_resultado.setObjectName("label_resultado")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.boton_convertir_dolares.setText(_translate("MainWindow", "Convertir a Dolares"))
+        self.boton_convertir_yenes.setText(_translate("MainWindow", "Convertir a Yenes"))
+        self.label.setText(_translate("MainWindow", "Cantidad en Euros:"))
