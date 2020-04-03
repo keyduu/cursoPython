9c4db91244f45374a77f1c779a912880d4bc5286
keyduu <keyduu@gmail.com>
2020-04-03 20:50:51 +0200

    Completado ejercicio

--- conversor_monedas/main.py
+++ conversor_monedas/main.py
@@ -11,18 +11,44 @@ import sys
 
 from PyQt5 import QtWidgets
 
-from ventanas import ventana_python
+from conversor_monedas.ventanas import ventana_python
 
 # ==== VARIABLES GLOBALES ==== #
 
 
 conversion_dolares = 1.11
 conversion_yenes = 119.15
+conversion_libras = 0.88
+conversion_rublos = 82.92
+conversion_bitcoins = 0.00016
 
 
 # ==== FUNCIONES ==== #
+def click_borrar():
+    ui.edit_euros.setText("0,00")
+    ui.grupo_botones.setExclusive(False)
+    ui.boton_convertir_bitcoins.setChecked(False)
+    ui.boton_convertir_yenes.setChecked(False)
+    ui.boton_convertir_dolares.setChecked(False)
+    ui.boton_convertir_libras.setChecked(False)
+    ui.boton_convertir_rublos.setChecked(False)
+    ui.grupo_botones.setExclusive(True)
+    ui.label_resultado.setText("")
+    ui.edit_resultado.setText("")
+
+
 def click_radio_button():
-    pass
+    if ui.boton_convertir_dolares.isChecked():
+        transformar_a_dolares()
+    elif ui.boton_convertir_yenes.isChecked():
+        transformar_a_yenes()
+    elif ui.boton_convertir_libras.isChecked():
+        transformar_a_libras()
+    elif ui.boton_convertir_rublos.isChecked():
+        transformar_a_rublos()
+    elif ui.boton_convertir_bitcoins.isChecked():
+        transformar_a_bitcoins()
+
 
 def transformar_a_dolares():
     introducido = ui.edit_euros.text()
@@ -39,6 +65,26 @@ def transformar_a_yenes():
     ui.label_resultado.setText("Cantidad(yenes): ")
     ui.edit_resultado.setText("{:.2f}".format(yenes).replace(".", ","))
 
+
+def transformar_a_libras():
+    introducido = ui.edit_euros.text()
+    introducido_float = float(introducido.replace(",", "."))
+    libras = introducido_float * conversion_libras
+    ui.label_resultado.setText("Cantidad(libras): ")
+    ui.edit_resultado.setText("{:.2f}".format(libras).replace(".", ","))
+
+def transformar_a_rublos():
+    introducido = ui.edit_euros.text()
+    introducido_float = float(introducido.replace(",", "."))
+    rublos = introducido_float * conversion_rublos
+    ui.label_resultado.setText("Cantidad(rublos): ")
+    ui.edit_resultado.setText("{:.2f}".format(rublos).replace(".", ","))
+def transformar_a_bitcoins():
+    introducido = ui.edit_euros.text()
+    introducido_float = float(introducido.replace(",", "."))
+    bitcoins = introducido_float * conversion_bitcoins
+    ui.label_resultado.setText("Cantidad(bitcoins): ")
+    ui.edit_resultado.setText("{:.2f}".format(bitcoins).replace(".", ","))
 
 # ==== MAIN ==== #
 
@@ -57,11 +103,11 @@ ui.setupUi(MainWindow)
 
 # todos los componentes puestos en la ventana por el designer estan ui
 ui.boton_convertir_dolares.toggled.connect(click_radio_button)
-ui.boton_convertir_yenes.toggle.connect(click_radio_button)
+ui.boton_convertir_yenes.toggled.connect(click_radio_button)
 ui.boton_convertir_libras.toggled.connect(click_radio_button)
 ui.boton_convertir_rublos.toggled.connect(click_radio_button)
-
-
+ui.boton_convertir_bitcoins.toggled.connect(click_radio_button)
+ui.boton_borrar.clicked.connect(click_borrar)
 
 # se muestra la ventana principal de PyQt5
 MainWindow.show()
